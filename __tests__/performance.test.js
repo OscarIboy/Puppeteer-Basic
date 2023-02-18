2c47f248e0a61aa83a3cb3387ad58e215c3b7d61
Oscar <oscar@r-digital.tech>
2023-02-18 01:04:07 -0600

    test performance

---
+++ __tests__/performance.test.js
@@ -0,0 +1,80 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+describe('Performance', () => {
+    let browser;
+    let page;
+
+	//beforeEach and beforeAll
+    beforeEach(async() =>{
+        browser = await puppeteer.launch({
+			headless: false,
+		});
+
+		page = await browser.newPage();
+
+    	// Set viewport width and height
+    	await page.setViewport({width: 1280, height: 720});
+    })
+
+    //afterEach and afterAll
+    afterEach(async ()=>{
+		await browser.close();
+    });
+
+    test('Medir el performance de la automatizacion', async () =>{
+        await page.goto('https://www.platzi.com/');
+        await page.waitForSelector('img');
+        const metrics = await page.metrics();
+        console.log(metrics);
+	}, 30000);
+
+    test('Medir el performance de la pagina', async () => {
+
+        await page.goto('https://www.platzi.com/');
+        await page.waitForSelector('img');
+        const metrics2 = await page.evaluate(() => JSON.stringify(window.performance));
+        console.log(metrics2);
+	}, 30000);
+
+    //test('Medir el performance del page load', async () => {
+    //    await page.tracing.start({ path: 'profile.json'});
+    //    await page.goto('https://www.platzi.com/');
+    //    await page.tracing.stop();
+	//}, 30000);
+
+    test('Medir el performance del page load con screenshots', async () => {
+
+        await page.tracing.start({ path: 'profile.json', screenshots: true});
+        await page.goto('https://www.platzi.com/');
+        await page.tracing.stop();
+	}, 30000);
+
+    test('Medir el performance del page load con screenshots y extraerlos', async () => {
+
+        await page.tracing.start({ path: 'profile.json', screenshots: true});
+        await page.goto('https://www.platzi.com/');
+        await page.tracing.stop();
+        
+        const tracing = JSON.parse(fs.readFileSync('./profile.json', 'utf-8'));
+
+        // Filtrar JSON
+        const traceScreenShots = tracing.traceEvents.filter(
+            (x) =>
+            x.cat === 'disabled-by-default-devtools.screenshot' &&
+            x.name === 'Screenshot' &&
+            typeof x.args !== 'undefined' &&
+            typeof x.args.snapshot !== 'undefined'
+        )
+
+        // Iterar Sobre este arreglo para crear las imagenes
+        traceScreenShots.forEach(function(snap, index){
+            fs.writeFile(`trace-screenshot-${index}.png`,snap.args.snapshot, 'base64', function(err){
+                if(err){
+                    console.log('No pude crear el archivo', err);
+                }
+            })
+        })
+
+	}, 30000);
+});
